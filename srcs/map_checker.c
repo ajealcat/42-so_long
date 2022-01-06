2319eeb5dd455abeb3494103d908b7f86a73d103
ajearuth <ajearuth@student.42.fr>
2022-01-06 22:59:09 +0100

    check if char used in the map are available

--- srcs/map_checker.c
+++ srcs/map_checker.c
@@ -6,27 +6,11 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 14:15:04 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/01/06 18:15:48 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/01/06 22:58:34 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-
-int	error_message(int i)
-{
-	if (i == 1)
-		ft_putstr_fd("Error\n Map must be surrounded by walls", 2);
-	if (i == 2)
-		ft_putstr_fd("Error\n Map must have at least one exit", 2);
-	if (i == 3)
-		ft_putstr_fd("Error\n Map must have at least one collectible", 2);
-	if (i == 4)
-		ft_putstr_fd("Error\n Map must have one starting position", 2);
-	if (i == 5)
-		ft_putstr_fd("Error\n Map must be rectangular", 2);
-	return (-1);
-}
 
 int	check_size(t_map map)
 {
@@ -139,4 +123,18 @@ int	check_e(t_map map)
 	if (count < 1)
 		return(error_message(2));
 	return(0);
+}
+
+int	is_available_entry(t_map map)
+{
+	int i;
+
+	i = 0;
+	while(map.map[i])
+	{
+		if(map.map[i] != '1' || map.map[i] != '0' || map.map[1] != 'P' 
+		|| map.map[i] != 'C' || map.map[i] != 'E')
+			return (error_message(6));
+	}
+	return (0);
 }
--- srcs/map_reader.c
+++ srcs/map_reader.c
@@ -6,11 +6,28 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 14:14:13 by ajearuth          #+#    #+#             */
-/*   Updated: 2022/01/06 18:05:45 by ajearuth         ###   ########.fr       */
+/*   Updated: 2022/01/06 22:58:15 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	error_message(int i)
+{
+	if (i == 1)
+		ft_putstr_fd("Error\n Map must be surrounded by walls", 2);
+	if (i == 2)
+		ft_putstr_fd("Error\n Map must have at least one exit", 2);
+	if (i == 3)
+		ft_putstr_fd("Error\n Map must have at least one collectible", 2);
+	if (i == 4)
+		ft_putstr_fd("Error\n Map must have one starting position", 2);
+	if (i == 5)
+		ft_putstr_fd("Error\n Map must be rectangular", 2);
+	if (i == 6)
+		ft_putstr_fd("Error\n Invalid instructions", 2);
+	return (-1);
+}
 
 t_map	init_struct_map(void)
 {
