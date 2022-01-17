1d1e780f26c77aa1d059e88f4cc1a415edd192af
ajearuth <ajearuth@student.42.fr>
2022-01-17 16:09:06 +0100

    textures must be ok

--- srcs/set_up_map.c
+++ srcs/set_up_map.c
@@ -15,13 +15,13 @@
 void	get_image(t_image *image, t_data data)
 {
 	image->wall_up = mlx_xpm_file_to_image(data.mlx_ptr, \
-	"textures/TX-Wall.xpm", &data.x, &data.y);
+	"textures/Wall.xpm", &data.x, &data.y);
 	image->wall_down = mlx_xpm_file_to_image(data.mlx_ptr, \
 	"textures/TX-down-Wall.xpm", &data.x, &data.y);
 	image->wall_side = mlx_xpm_file_to_image(data.mlx_ptr, \
 	"textures/TX-Side-Wall.xpm", &data.x, &data.y);
 	image->grass = mlx_xpm_file_to_image(data.mlx_ptr, \
-	"textures/TX-Grass.xpm", &data.x, &data.y);
+	"textures/Grass.xpm", &data.x, &data.y);
 	image->door = mlx_xpm_file_to_image(data.mlx_ptr, \
 	"textures/TX-.xpm", &data.x, &data.y);
 }
@@ -37,15 +37,15 @@ void	put_on_screen(t_data data, t_map map, t_image image)
 		j = 0;
 		while(map.mappy[i][j])
 		{
-			if(map.mappy[i][j] == '0')
-			{
-				mlx_put_image_to_window(data.mlx_ptr, \
-				data.window_ptr, image.grass, j * 48, i * 48);
-			}
-			else if (map.mappy[i][j] == '1')
+			if (map.mappy[i][j] == '1')
 			{
 				mlx_put_image_to_window(data.mlx_ptr, \
 				data.window_ptr, image.wall_up, j * 48, i * 48);
+			}
+			else 
+			{
+				mlx_put_image_to_window(data.mlx_ptr, \
+				data.window_ptr, image.grass, j * 48, i * 48);
 			}
 			++j;
 		}
